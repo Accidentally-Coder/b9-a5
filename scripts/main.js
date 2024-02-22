9d87cb2eea339be0aecb9bbffec4d0fe2feb7eab
Farzana Reefat Raha <reefat.raha2018@gmail.com>
2024-02-22 19:05:57 +0600

    Changed selected button to green

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,13 @@ function selectedSeat(seatId) {
     console.log('pressed ', seatId);
     noSeatSelected = noSeatSelected + 1;
     console.log(noSeatSelected);
+    // changinn "n Seats Left" text
     seatLeftId = document.getElementById("seatLeftId");
-    seatLeftId.innerHTML = (noTotalSeat - noSeatSelected) + " Seats Left"
+    seatLeftId.innerHTML = (noTotalSeat - noSeatSelected) + " Seats Left";
+    // changing button color to green
+    const seatSelected = document.getElementById(seatId);
+    seatSelected.classList.add('bg-[#1DD100]')
+    seatSelected.classList.replace('text-gray-400', 'text-white');
+    console.log(seatSelected.classList);
+
 }
