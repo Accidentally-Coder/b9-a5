c6ab1c337b5a1dff23b191da7e2ce3a36ce06a32
Farzana Reefat Raha <reefat.raha2018@gmail.com>
2024-02-22 19:25:19 +0600

    Increased total price and grand total price if seat selected

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,19 @@
 let noSeatSelected = 0;
 const noTotalSeat = 8
+let totalPrice = 0;
+let grandTotalPrice = 0;
 
 function selectedSeat(seatId) {
     console.log('pressed ', seatId);
+    // if more than 4 seats selected
+    if (noSeatSelected > 4) {
+        console.log("You cannot select more than 4");
+    }
     noSeatSelected = noSeatSelected + 1;
+
+    totalPrice = totalPrice + 550;
+    grandTotalPrice = grandTotalPrice + 550;
+
     console.log(noSeatSelected);
     // changinn "n Seats Left" text
     seatLeftId = document.getElementById("seatLeftId");
@@ -16,4 +26,11 @@ function selectedSeat(seatId) {
     // changing table "selected-seat-id" number
     const tableBubble = document.getElementById("selected-seat-id");
     tableBubble.innerHTML = noSeatSelected;
+    // increase total price "total-price"
+    const totalPriceElement = document.getElementById("total-price");
+    totalPriceElement.innerHTML = totalPrice;
+    // increase grand total "grand-total"
+    const grandTotalElement = document.getElementById("grand-total");
+    grandTotalElement.innerHTML = grandTotalPrice;
+
 }
